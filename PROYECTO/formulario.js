95bdc40ee5a5a721016aa1259dfc07373fec6836
Marcos Hazael Guzman Vargas <168611614+HazaelGuvm@users.noreply.github.com>
2024-07-30 14:15:22 -0600

    Add files via upload

---
+++ PROYECTO/formulario.js
@@ -0,0 +1,6 @@
+var nombres = [
+    { nombre: "Marcos", saldo: 500, contrasena: "marcos" },
+    { nombre: "Vico", saldo: 4500, contrasena: "vico" },
+    { nombre: "Lujan", saldo: 800, contrasena: "lujan" },
+];
+var cuentaActual = null;
